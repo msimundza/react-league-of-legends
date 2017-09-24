20eb17c6aeca9f12fd5aba29e08c87eab1e1e5f4
Marko Šimundža <markosimundza@gmail.com>
2017-09-24 21:29:31 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import config from './config'
 
+const champListApi = `https://eun1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false&api_key=${config.API_KEY}`
+
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      championList: []
+    }
+  }
+  componentWillMount () {
+    fetch(champListApi)
+      .then(results => {
+        return results.json()
+      }).then(data => {
+        this.setState({championList: data})
+      })
+  }
   render () {
     return (
       <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to React</h2>
-          <h2>{config.API_KEY}</h2>
-        </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>!!!!!!!!!!!!!!!!!!</div>
+        <div>{this.state.championList}</div>
       </div>
     )
   }
